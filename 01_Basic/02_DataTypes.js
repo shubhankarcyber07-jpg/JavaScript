5e9b1711850903b2d07b805e927ce3ea972d9306
Shubhankar Sasmal <shubhankarcyber07@gmail.com>
2026-09-18 17:16:28 +0530

    coversion

--- 01_Basic/02_DataTypes.js
+++ 01_Basic/02_DataTypes.js
@@ -6,22 +6,22 @@ console.log(3+3);
 console.log(10-3);
 
 let age = 25; // number
-console.log(typeof age);
+console.log(typeof age); // number
 
 let firstName = "Shubhankar"; // string
-console.log(typeof firstName);
+console.log(typeof firstName); // string
 
 let isStudent = true; // boolean
-console.log(typeof isStudent);
+console.log(typeof isStudent); // boolean
 
-let lastName = null; // null
-console.log(typeof lastName);
+let lastName = null; // null 
+console.log(typeof lastName); //object
 
 let middleName; // undefined
-console.log(typeof middleName);
+console.log(typeof middleName); // undefined
 
 let num = 1234567890123456789012345678901234567890n; // bigint
-console.log(typeof num);
+console.log(typeof num); // bigint
 
 // number => 2 to power of 53 - 1
 // bigint => 2 to power of 63 - 1
